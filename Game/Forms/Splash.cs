5fd1357f630393a7d33c1f3573d60aeebe8f194c
Jordan Link <jordanlink1991@gmail.com>
2022-05-04 15:08:54 -0500

    Initial commit

---
+++ Game/Forms/Splash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.Forms
+{
+	public partial class Splash : Form
+	{
+		public Splash()
+		{
+			InitializeComponent();
+		}
+		
+
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			base.OnPaintBackground(e);
+		}
+	}
+}
